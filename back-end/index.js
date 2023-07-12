df68afdb4051df4b4c13addc4288c9903f89f83a
EL HOUSSINE ZARHOUNE <94381264+zarhouneNotes@users.noreply.github.com>
2023-07-12 20:47:39 +0200

    Update socket origin

--- back-end/index.js
+++ back-end/index.js
@@ -37,7 +37,7 @@ const  {Server} = require('socket.io')
 
 const io = new Server(server ,{
     cors :{
-        origin : 'http://localhost:3000',
+        origin : ["https://sheesapp.onrender.com"  , 'http://localhost:3000'],
         methods: ["GET", "POST"],
     }
 })
